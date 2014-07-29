2d1f71cc9e6d0b6b825f4c3afa0848ec220a0a5a
Francisco Sant'Anna <francisco.santanna@gmail.com>
2014-07-29 14:15:08 -0300

    (+) timer.cancel (^) fut.fg() => fut.fg_done()

--- FreeRTOS_Posix/app5.lua
+++ FreeRTOS_Posix/app5.lua
@@ -2,7 +2,8 @@ dofile '../ext_timer.lua'
 
 local main = coroutine.wrap(
     function ()
-        timer.cb(1000, function() print'1000' end)
+        local t = timer.cb(1000, function() print'1000' end)
+        t.cancel()
         timer.cb(2000, function()
             print'2000'
             timer.cb(2000, function ()
@@ -15,7 +16,11 @@ local main = coroutine.wrap(
         local f = timer.bg(2000)
         timer.fg(1000)
         print'>2000'
-        f.fg()
+        assert(f.fg_done())
         print'>3000'
+
+        local f = timer.bg(1000)
+        assert(f.cancel())
+        assert(f.fg_done(), 'cancelled')
     end)
 main()
--- FreeRTOS_Posix/ext_timer.lua
+++ FreeRTOS_Posix/ext_timer.lua
@@ -12,10 +12,11 @@ local __cb = function ()
 
     for i=1, #__queue do
         local t = __queue[i]
-        local dt, cb = unpack(t)
+        local dt, cb, alive = unpack(t)
         dt = dt - 1
         t[1] = dt
-        if dt <= 0 then
+        if (dt <= 0) and alive then
+            t[3] = false    -- avoids re-execution on error
             cb()
         end
     end
@@ -30,37 +31,64 @@ events.listen('EVT_TIMER_IN_EXPIRED', __cb)
 events.post('EVT_TIMER_OUT_START', 100)
 
 function timer.cb (dt, cb)
-    __queue[#__queue+1] = { dt/100, cb}
+    local t = { dt/100, cb, true }
+    __queue[#__queue+1] = t
     table.sort(__queue, __sort)
+    return {
+        cancel = function ()
+                    t[3] = false    -- cancel timer
+                  end
+    }
 end
 
 function timer.fg (dt)
     local co = coroutine.running()
-    timer.cb(dt, function ()
+    local t = timer.cb(dt, function ()
         assert(coroutine.resume(co))
     end)
-    coroutine.yield()
+    coroutine.yield(t)
 end
 
 function timer.bg (dt)
-    local state = nil   -- nil, 'done', co
-    coroutine.wrap(function ()
+    local state = nil   -- nil, 'done', 'cancelled', co
+    local co = coroutine.create(function ()
         timer.fg(dt)
         if state == nil then
             state = 'done'
         else
             assert(state ~= 'done')
-            coroutine.resume(state)
+            assert(coroutine.resume(state,true))
         end
-    end)()
+    end)
+    local ok, t = coroutine.resume(co)      -- result of timer.cb()
+    assert(ok, t)
     return {
-        fg = function ()
+        fg_done = function ()
             if state == nil then
                 state = coroutine.running()
-                coroutine.yield()
+                return coroutine.yield()    -- wait result
+            elseif state == 'done' then
+                return true                 -- already terminated ok
             else
-                assert(state == 'done')
+                assert(state == 'cancelled')
+                return false                -- already cancelled
             end
-        end
+        end,
+        cancel = function ()
+            t.cancel()                      -- cancel cb
+            if state == 'done' then
+                return false                -- already done
+            elseif state == 'cancelled' then
+                return true                 -- already cancelled, but ok
+            elseif state == nil then
+                state = 'cancelled'
+                return true                 -- cancelled
+            else
+                assert( type(state)=='thread' )
+                assert(coroutine.resume(state,false))
+                state = 'cancelled'
+                return true                 -- cancelled
+            end
+        end,
     }
 end
